2852924d31919b881cbdab21fe7df8cbe219ff71
hex248 <oliverbryan248@gmail.com>
2020-06-24 15:06:04 +0100

    OLIVER'S COMMENT

--- gpmachine.py
+++ gpmachine.py
@@ -8,23 +8,24 @@ canvasHeight = 500
 buttonWidth = 120
 buttonHeight = 120
 
-def home():
-    trigonometryButton.place()
-    entry.place()
-    label.place()
-
 def button(value):
     print("Button Pressed:", value)
     
     trigonometryButton.place_forget()
     entry.place_forget()
     label.place_forget()
-
-    backButton = tk.Button(frame, text="Back", bg="lightblue", font="60", command=lambda: button("back"))
     backButton.place(x="5", y="5", width=buttonWidth, height=buttonHeight)
 
     if value == "back":
         home()
+
+def home():
+    backButton.place_forget()
+    trigonometryButton.place(x="5", y="5", width=buttonWidth, height=buttonHeight)
+    entry.place(x="250", y="250")
+    entry.pack()
+    label.place(x="250", y="250")
+    entry.pack()
 
 canvas = tk.Canvas(root, height=canvasHeight, width=canvasWidth)
 canvas.pack()
@@ -35,9 +36,13 @@ frame.place(relwidth="1", relheight="1")
 trigonometryButton = tk.Button(frame, text="Trigonometry", bg="lightblue", font="60", command=lambda: button("trigonometry"))
 trigonometryButton.place(x="5", y="5", width=buttonWidth, height=buttonHeight)
 
+backButton = tk.Button(frame, text="Back", bg="lightblue", font="60", command=lambda: button("back"))
+
 entry = tk.Entry(frame, bg="white")
 entry.pack()
 entry.place(x="250", y="250")
+
+# OLIVER'S COMMENT
 
 label = tk.Label(frame, text="Type Below", bg="lightblue")
 label.pack()
