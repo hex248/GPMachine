34fb47d48ba8a63c2889669d183b1db6966e4c1b
hex248 <oliverbryan248@gmail.com>
2020-06-24 21:02:38 +0100

    Added gray to saved colours

--- gpmachine.py
+++ gpmachine.py
@@ -14,6 +14,7 @@ buttonHeight = 118.75
 
 mColour = "#36b3ac"
 bColour = "#b52126"
+gColour = "#808080"
 
 def button(value):
     print("Button Pressed:", value)
@@ -68,7 +69,7 @@ trigLabel = tk.Label(root, image = trigImage)
 
 
 
-frame = tk.Frame(root, bg='gray')
+frame = tk.Frame(root, bg='gColour')
 frame.place(relwidth=1, relheight=1, anchor='n', relx = 0.5)
 
 def trig():
