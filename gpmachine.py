4daee6cb2d7bc3b3e1b96d22881fb905f287ba5e
hex248 <oliverbryan248@gmail.com>
2020-06-24 16:16:23 +0100

    Added all 16 buttons and changed colour

--- gpmachine.py
+++ gpmachine.py
@@ -8,12 +8,27 @@ canvasHeight = 500
 buttonWidth = 118.75
 buttonHeight = 118.75
 
+mColour = "#36b3ac"
+
 def button(value):
     print("Button Pressed:", value)
-    
     trigonometryButton.place_forget()
-    TesttryButton.place_forget()
-
+    button2.place_forget()
+    button3.place_forget()
+    button4.place_forget()
+    button5.place_forget()
+    button6.place_forget()
+    button7.place_forget()
+    button8.place_forget()
+    button9.place_forget()
+    button10.place_forget()
+    button11.place_forget()
+    button12.place_forget()
+    button13.place_forget()
+    button14.place_forget()
+    button15.place_forget()
+    button16.place_forget()
+    
     backButton.place(x="5", y="5", width=buttonWidth, height=buttonHeight)
 
     if value == "back":
@@ -22,22 +37,78 @@ def button(value):
 def home():
     backButton.place_forget()
     trigonometryButton.place(x="5", y="5", width=buttonWidth, height=buttonHeight)
-    TesttryButton.place(x="128.75", y="5", width=buttonWidth, height=buttonHeight)
-
-
+    button2.place(x="128.75", y="5", width=buttonWidth, height=buttonHeight)
+    button3.place(x="252.5", y="5", width=buttonWidth, height=buttonHeight)
+    button4.place(x="376.25", y="5", width=buttonWidth, height=buttonHeight)
+    button5.place(x="5", y="128.75", width=buttonWidth, height=buttonHeight)
+    button6.place(x="128.75", y="128.75", width=buttonWidth, height=buttonHeight)
+    button7.place(x="252.5", y="128.75", width=buttonWidth, height=buttonHeight)
+    button8.place(x="376.25", y="128.75", width=buttonWidth, height=buttonHeight)
+    button9.place(x="5", y="252.5", width=buttonWidth, height=buttonHeight)
+    button10.place(x="128.75", y="252.5", width=buttonWidth, height=buttonHeight)
+    button11.place(x="252.5", y="252.5", width=buttonWidth, height=buttonHeight)
+    button12.place(x="376.25", y="252.5", width=buttonWidth, height=buttonHeight)
+    button13.place(x="5", y="376.25", width=buttonWidth, height=buttonHeight)
+    button14.place(x="128.75", y="376.25", width=buttonWidth, height=buttonHeight)
+    button15.place(x="252.5", y="376.25", width=buttonWidth, height=buttonHeight)
+    button16.place(x="376.25", y="376.25", width=buttonWidth, height=buttonHeight)
 canvas = tk.Canvas(root, height=canvasHeight, width=canvasWidth)
 canvas.pack()
 
 frame = tk.Frame(root, bg='gray')
 frame.place(relwidth="1", relheight="1")
 
-trigonometryButton = tk.Button(frame, text="Trigonometry", bg="lightblue", font="60", command=lambda: button("trigonometry"))
-trigonometryButton.place(x="5", y="5", width=buttonWidth, height=buttonHeight)
+if True: #ALL BUTTONS
+    trigonometryButton = tk.Button(frame, text="Trigonometry", bg=mColour, font="60", command=lambda: button("trigonometry"))
+    trigonometryButton.place(x="5", y="5", width=buttonWidth, height=buttonHeight)
 
-TesttryButton = tk.Button(frame, text="Test", bg="Blue", font="60", command=lambda: button("Test"))
-TesttryButton.place(x="128.75", y="5", width=buttonWidth, height=buttonHeight)
+    button2 = tk.Button(frame, text="button2", bg=mColour, font="60", command=lambda: button("button2"))
+    button2.place(x="128.75", y="5", width=buttonWidth, height=buttonHeight)
 
-backButton = tk.Button(frame, text="Back", bg="lightblue", font="60", command=lambda: button("back"))
+    button3 = tk.Button(frame, text="button3", bg=mColour, font="60", command=lambda: button("button3"))
+    button3.place(x="252.5", y="5", width=buttonWidth, height=buttonHeight)
+
+    button4 = tk.Button(frame, text="button4", bg=mColour, font="60", command=lambda: button("button4"))
+    button4.place(x="376.25", y="5", width=buttonWidth, height=buttonHeight)
+
+    button5 = tk.Button(frame, text="button5", bg=mColour, font="60", command=lambda: button("button5"))
+    button5.place(x="5", y="128.75", width=buttonWidth, height=buttonHeight)
+
+    button6 = tk.Button(frame, text="button6", bg=mColour, font="60", command=lambda: button("button6"))
+    button6.place(x="128.75", y="128.75", width=buttonWidth, height=buttonHeight)
+
+    button7 = tk.Button(frame, text="button7", bg=mColour, font="60", command=lambda: button("button7"))
+    button7.place(x="252.5", y="128.75", width=buttonWidth, height=buttonHeight)
+
+    button8 = tk.Button(frame, text="button8", bg=mColour, font="60", command=lambda: button("button8"))
+    button8.place(x="376.25", y="128.75", width=buttonWidth, height=buttonHeight)
+
+    button9 = tk.Button(frame, text="button9", bg=mColour, font="60", command=lambda: button("button9"))
+    button9.place(x="5", y="252.5", width=buttonWidth, height=buttonHeight)
+
+    button10 = tk.Button(frame, text="button10", bg=mColour, font="60", command=lambda: button("button10"))
+    button10.place(x="128.75", y="252.5", width=buttonWidth, height=buttonHeight)
+
+    button11 = tk.Button(frame, text="button11", bg=mColour, font="60", command=lambda: button("button11"))
+    button11.place(x="252.5", y="252.5", width=buttonWidth, height=buttonHeight)
+
+    button12 = tk.Button(frame, text="button12", bg=mColour, font="60", command=lambda: button("button12"))
+    button12.place(x="376.25", y="252.5", width=buttonWidth, height=buttonHeight)
+
+    button13 = tk.Button(frame, text="button13", bg=mColour, font="60", command=lambda: button("button13"))
+    button13.place(x="5", y="376.25", width=buttonWidth, height=buttonHeight)
+
+    button14 = tk.Button(frame, text="button14", bg=mColour, font="60", command=lambda: button("button14"))
+    button14.place(x="128.75", y="376.25", width=buttonWidth, height=buttonHeight)
+
+    button15 = tk.Button(frame, text="button15", bg=mColour, font="60", command=lambda: button("button15"))
+    button15.place(x="252.5", y="376.25", width=buttonWidth, height=buttonHeight)
+
+    button16 = tk.Button(frame, text="button16", bg=mColour, font="60", command=lambda: button("button16"))
+    button16.place(x="376.25", y="376.25", width=buttonWidth, height=buttonHeight)
+
+    backButton = tk.Button(frame, text="Back", bg="lightblue", font="60", command=lambda: button("back"))
+
 
 #end of script
 
