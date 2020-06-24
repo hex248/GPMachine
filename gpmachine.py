6edd690a6f428a708fcdcc269bc8e2761f79ab3f
hex248 <65399431+hex248@users.noreply.github.com>
2020-06-24 21:11:49 +0100

    fix

--- gpmachine.py
+++ gpmachine.py
@@ -14,6 +14,7 @@ buttonHeight = 118.75
 
 mColour = "#36b3ac"
 bColour = "#b52126"
+gColour = "#808080"
 
 def button(value):
     print("Button Pressed:", value)
@@ -69,7 +70,7 @@ trigLabelA = tk.Label(font = '60', text = 'a', fg = 'black', bg = 'white')
 
 
 
-frame = tk.Frame(root, bg='gray')
+frame = tk.Frame(root, bg='gColour')
 frame.place(relwidth=1, relheight=1, anchor='n', relx = 0.5)
 
 def trig():
